90e3d7fd72ededda0480ae8d64fa1694f21adfe5
James Rees <jarees@gmail.com>
2018-07-26 23:26:36 +0200

    Remove (now invalidated) security credentials

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAJYEZ52WRMXS2E2PQ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
