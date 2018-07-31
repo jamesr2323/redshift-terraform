744e335725a7062486cf95860826f8fd23c99676
Rich Shakespeare <richshakespeare@hotmail.co.uk>
2018-07-31 10:47:55 +0100

    add environment variable (default to Dev)

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "organisation_name" {
 	description = "A unique prefix, representing your organisation's name, which we'll prepend to created resources."
 	type = "string"
 }
+
+variable "environment" {
+  description = "The environment you're running this for. Defaults to 'Dev'"
+  type = "string"
+  default = "Dev"
+}
