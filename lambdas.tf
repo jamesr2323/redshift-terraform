8d835ba395a14d6570cf3f2a695f4839feefabef
Rich Shakespeare <richshakespeare@hotmail.co.uk>
2018-07-31 12:56:48 +0100

    correctly create lambda from zip file using correct function name

--- lambdas.tf
+++ lambdas.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "copy_to_s3" {
-  filename          = "lambda_scripts/copy_to_s3.js"
+  filename          = "lambda_scripts/bundles/copy_to_s3.js.zip"
   role             = "${aws_iam_role.iam_for_copy_to_s3_lambda.arn}"
-  handler          = "exports.test"
+  handler          = "copy_to_s3.handler"
   runtime          = "nodejs8.10"
-  function_name    = "copyToS3"
+  function_name    = "CopyToS3"
   timeout          = 30
   environment {
     variables = {
@@ -13,12 +13,12 @@ resource "aws_lambda_function" "copy_to_s3" {
 }
 
 resource "aws_lambda_function" "load_to_redshift_lambda" {
-  s3_bucket         = "awslabs-code-us-east-1"
+  s3_bucket         = "awslabs-code-${var.aws_region}"
   s3_key            = "LambdaRedshiftLoader/AWSLambdaRedshiftLoader-2.5.0.zip"
   role             = "${aws_iam_role.iam_for_redshift_lambda.arn}"
-  handler          = "exports.test"
+  handler          = "exports.handler"
   runtime          = "nodejs8.10"
-  function_name    = "loadToRedshift"
+  function_name    = "LambdaRedshiftLoader" // required for automated event handling
   environment {
     variables = {
     }
